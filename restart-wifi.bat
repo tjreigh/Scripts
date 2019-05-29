43102bd038eeaa8e0aee22d02e17907fc89590e1
TJ The Developer <tjdoescode@gmail.com>
2019-05-29 16:32:22 -0500

    Add wifi adapter script

---
+++ restart-wifi.bat
@@ -0,0 +1,39 @@
+::==============================================================================
+:: This script is an automation to fix the random Wi-Fi dropouts on my computer
+:: 
+:: Author: TJDoesCode
+:: 5/29/2019
+::==============================================================================
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+
+:loop
+:: State detection loop - just pings google
+set state=up
+ping -n 1 www.google.com >nul: 2>nul:
+if not !errorlevel!==0 set state=down
+echo %TIME:~0,-1% ^| Link is !state!
+if !state!==down goto :reset
+ping -n 2 www.google.com >nul: 2>nul:
+timeout /t 5 >nul
+goto :loop
+endlocal
+
+:reset
+:: Reset network adapter
+echo Resetting Wi-Fi interface
+netsh interface set interface "Wi-Fi" DISABLED
+timeout /t 2 >nul
+netsh interface set interface "Wi-Fi" ENABLED
+timeout /t 15 >nul
+:: Check if connection up
+WMIC /node: "TJ-HotRod" path WIN32_NetworkAdapter where (NetConnectionID="Wi-Fi") get NetConnectionStatus | find /c "2" >NUL
+if %ERRORLEVEL% EQU 0 ( 
+    echo %TIME:~0,-1% ^| Link back up
+    timeout /t 2 >nul
+    goto :loop
+) else ( 
+    echo %TIME:~0,-1% ^| Link still not up
+    timeout /t 2 >nul
+    goto :reset
+)
